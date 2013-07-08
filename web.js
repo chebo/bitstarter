3e6e58001757d1e1900f88d4fd7815bc5657bb6b
Grigoriy Chebotarev <g.chebo@gmail.com>
2013-07-08 21:43:18 +0000

    hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+fs=require("fs");
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync('index.html');
+
+var buffer = new Buffer(file, "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
